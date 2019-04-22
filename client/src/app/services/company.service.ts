a2410379924b9427d2e0cf321d0454ab1c040658
amcfire <amendez.contac@gmail.com>
2019-04-22 07:55:53 -0500

    update paths

--- client/src/app/services/company.service.ts
+++ client/src/app/services/company.service.ts
@@ -1,8 +1,8 @@
 ﻿import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-import { environment } from '../environments/environment';
-import { Company } from '../_models';
+import { environment } from '../../environments/environment';
+import { Company } from '../models';
 
 @Injectable()
 export class CompanyService {
