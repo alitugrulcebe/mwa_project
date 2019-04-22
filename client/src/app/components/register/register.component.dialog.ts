58437a623473cf0c51d08de378eefe52771a7cbf
Ali Tugrul Cebe <>
2019-04-21 23:01:36 -0500

    Main screen with Material UI

---
+++ client/src/app/components/register/register.component.dialog.ts
@@ -0,0 +1,67 @@
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+//import {AuthService} from "../services/auth.service";
+import {Router} from "@angular/router";
+import {AuthService} from "../../services/auth.service";
+import {MatDialogRef, MatSnackBar} from "@angular/material";
+
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.dialog.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponentDialog implements OnInit {
+  myForm: FormGroup;
+  error:string | null
+  constructor(public dialogRef: MatDialogRef<RegisterComponentDialog>,
+              private formBuilder:FormBuilder,
+              private authService:AuthService,
+              private router:Router,
+              private snackBar: MatSnackBar) { //private client:AuthService
+    this.myForm = formBuilder.group({
+      'userData': formBuilder.group({
+        'firstname': ['First Name'],
+        'lastname': ['Last Name'],
+        'email': ['', [
+          Validators.required,
+          Validators.pattern("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")
+        ]],
+        'password': ['', Validators.required],
+        'cpassword': ['', Validators.required],
+        'term':['']
+      })
+    });
+
+    // this.myForm.statusChanges.subscribe(
+    //   (data: any) => console.log(data)
+    // );
+  }
+
+  onSubmit() {
+    const user = {
+      firstname:this.myForm.value.userData.firstname,
+      lastname:this.myForm.value.userData.lastname,
+      email:this.myForm.value.userData.email,
+      password:this.myForm.value.userData.password,
+    };
+    this.authService.register(user).subscribe(res => {
+      this.authService.login(user.email,user.password).subscribe(res => {
+        debugger;
+        console.log(res);
+        localStorage.setItem('userData',JSON.stringify({ token:res['token']}));
+        this.authService.setLoggedIn(true);
+        this.snackBar.open("Registration successful","Success",{duration:5000,direction:"ltr"});
+        this.dialogRef.close();
+        // Navigate to the login page with extras
+        this.router.navigate(['/']);
+      });
+
+    });;
+
+  }
+
+  ngOnInit() {
+  }
+
+}
---
+++ client/src/app/environment/environment.ts
@@ -0,0 +1,5 @@
+// environment.ts
+export const environment = {
+  production: false,
+  server:'http://localhost:3000'
+};
