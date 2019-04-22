77a30ebfd9a2ff3384171e02549a99576e8bcd8b
amcfire <amendez.contac@gmail.com>
2019-04-22 17:09:31 -0500

    comment city

--- client/src/app/environment/environment.prod.ts
+++ client/src/app/environment/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:3000'
+  server: 'http://localhost:3000'
 };
--- server/routes/protected.js
+++ server/routes/protected.js
@@ -4,13 +4,13 @@ const bcrypt = require('bcrypt');
 const User = require('../models/users'); // get our mongoose model
 const app = express.Router();
 var mongoose = require('mongoose');
-const City = require('city');
+//const City = require('city');
 require('dotenv').config();
 var port = process.env.PORT || 8080; // used to create, sign, and verify tokens
 mongoose.connect(process.env.DATABASE, { useNewUrlParser: true });
 
 
-
+/*
 app.post('/city', function (req, res, next) {
     City.find({}, { 'name': req.body.name })
         .exec()
@@ -20,6 +20,6 @@ app.post('/city', function (req, res, next) {
             })
         });
 });
-
+*/
 
 module.exports = app;
